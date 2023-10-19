fc1772cae51dee2a0ccf79139fd83c5f50f20ad7
neerojstha <nirojstha@hotmail.com>
2023-10-19 00:40:44 +0000

    checkout template forms added

--- checkout/models.py
+++ checkout/models.py
@@ -1,11 +1,11 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
