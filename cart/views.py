3e0e28b5d44f004da51d0edfb1baea411c2ad406
neerojstha <nirojstha@hotmail.com>
2023-10-20 23:57:16 +0000

    cart typo error corrected

--- cart/views.py
+++ cart/views.py
@@ -45,20 +45,26 @@ def adjust_cart(request, item_id):
 
 
 def remove_from_cart(request, item_id):
-    """Remove the item from the shopping bag."""
+    """Remove the item from the shopping cart"""
 
     try:
         product = get_object_or_404(Product, pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
         cart = request.session.get('cart', {})
 
-        if item_id in cart:
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your cart')
+        else:
             cart.pop(item_id)
             messages.success(request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
-
-        # Redirect to the URL with a trailing slash
-        return redirect('remove_from_cart', item_id=item_id)
+        return HttpResponse(status=200)
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
