04eb0183e894c56cea7ff9fd2acda8f4d5432bef
neerojstha <nirojstha@hotmail.com>
2023-10-22 14:46:28 +0000

    email setup completed

--- wings_gym/settings.py
+++ wings_gym/settings.py
@@ -181,3 +181,4 @@ STRIPE_CURRENCY = 'euro'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'wingsgym@example.com'
