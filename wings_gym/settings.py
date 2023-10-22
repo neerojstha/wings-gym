d29cafdc52f27d054b5e17ff1ed9edc78dc0e6e0
neerojstha <nirojstha@hotmail.com>
2023-10-22 16:35:19 +0000

    procfile created

--- wings_gym/settings.py
+++ wings_gym/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,11 +118,23 @@ WSGI_APPLICATION = 'wings_gym.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+DATABASES = {
+     'default': dj_database_url.parse('postgres://ohkmxnwp:MEw66Cj_YFnD6GTU_xryRjG65-8Wmi70@flora.db.elephantsql.com/ohkmxnwp')
 }
 
 APPEND_SLASH=False
