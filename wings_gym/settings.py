fa38d4845fc278e765eddc210311eaea06d3773e
neerojstha <nirojstha@hotmail.com>
2023-10-22 17:06:59 +0000

    removed secret key

--- wings_gym/settings.py
+++ wings_gym/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--ltai_%qh)br3tg&6&)g!g9$n_d!w&73y27dpo6ppnj2&4f1sq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wingsgym.herokuapp.com', 'localhost']
 
