d5e221ea27439390f7507766c4b2833201ab22be
neerojstha <nirojstha@hotmail.com>
2024-01-05 12:10:29 +0000

    checkout views file updated

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from django.conf import settings
 from django.views.decorators.http import require_POST
@@ -9,6 +9,7 @@ from cart.contexts import cart_contents
 from products.models import Product
 from .models import Order, OrderLineItem
 import stripe
+import json
 
 @require_POST
 def cache_checkout_data(request):
