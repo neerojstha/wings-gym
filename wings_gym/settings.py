325725b607303622a8a12312ab18a5ef637c1759
neerojstha <nirojstha@hotmail.com>
2024-01-07 23:59:24 +0000

    deployment commit

--- wings_gym/settings.py
+++ wings_gym/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-neerojstha-wingsgym-2r0xmbf9eql.ws-eu107.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['wingsgym-20a4a87d8885.herokuapp.com', 'localhost']
 
 
 # Application definition
