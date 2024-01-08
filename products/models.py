614ce3eb51012fa1b5399da2cae7a65f095d6eca
neerojstha <nirojstha@hotmail.com>
2024-01-08 12:12:55 +0000

    deployment part 3

--- products/models.py
+++ products/models.py
@@ -42,7 +42,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     # Other product-related fields
     
@@ -56,7 +55,6 @@ class NutritionPlan(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
